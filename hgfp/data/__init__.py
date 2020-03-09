b7dd5bab8104f6cb32eb23b889baece2d700143c
yuanqing-wang <wangyq@wangyq.net>
2020-03-09 13:39:14 -0400

    data input pipeline

--- hgfp/data/__init__.py
+++ hgfp/data/__init__.py
@@ -2,3 +2,4 @@ import hgfp.data.qm9
 import hgfp.data.qc_archive
 import hgfp.data.utils
 import hgfp.data.parm_at_Frosst
+import hgfp.data.ani
--- hgfp/data/qc_archive/qc_download_helper.py
+++ hgfp/data/qc_archive/qc_download_helper.py
@@ -20,39 +20,64 @@ import cmiles
 from simtk import openmm
 import random
 import numpy as np
-
-
+from multiprocessing import Pool
+import itertools
+from dgl import data
 ds_qc = client.get_collection("OptimizationDataset", "OpenFF Full Optimization Benchmark 1")
 
+
+import signal
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
 # initialize graph list to be empty
 
 records = list(ds_qc.data.records)
 
-global ds
-gs = []
-us = []
-
-for record_name in records:
+def f(record_name):
     try:
+            print(record_name, flush=True)
+            us = []
+            gs = []
             r = ds_qc.get_record(record_name, specification='default')
-
             if r is not None:
                 traj = r.get_trajectory()
                 if traj is not None:
                     for snapshot in traj:
-                        energy = snapshot.properties.scf_total_energy
-                        mol = snapshot.get_molecule()
+                        with timeout(seconds=5):
+                            energy = snapshot.properties.scf_total_energy
+                            mol = snapshot.get_molecule()
 
-                        mol = cmiles.utils.load_molecule(mol.dict(encoding='json'),
-                            toolkit='rdkit')
+                            mol = cmiles.utils.load_molecule(mol.dict(encoding='json'),
+                                toolkit='rdkit')
 
-                        u = torch.squeeze(torch.Tensor([energy]))
-                        g = hgfp.graph.from_rdkit_mol(mol)
+                            u = torch.squeeze(torch.Tensor([energy]))
+                            g = hgfp.graph.from_rdkit_mol(mol)
 
-                        gs.append(g)
-                        us.append(u)
+                            us.append(u)
+                            gs.append(g)
+
+            return us, gs
     except:
-        continue
+        pass
+            
 
+if __name__ == '__main__':
+    p = Pool(32)
+    us_gs_array = p.map(f, records)
+    us = torch.stack(list(itertools.chain.from_iterable([x[0] for x in us_gs_array if x is not None])))
+    gs = list(itertools.chain.from_iterable([x[1] for x in us_gs_array if x is not None]))
 
-dgl.data.utils.save_graphs('qc_archive.bin', gs, {'u': torch.stack(us)})
+    dgl.data.utils.save_graphs('qc_archive_1.bin', gs, {'u': us})
