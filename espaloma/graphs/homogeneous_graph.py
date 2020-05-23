cf748839ed6cd182c7d7d250c1f46e8327e1d513
yuanqing-wang <wangyq@wangyq.net>
2020-05-23 15:38:40 -0400

    restructure graphs

--- espaloma/graphs/homogeneous_graph.py
+++ espaloma/graphs/homogeneous_graph.py
@@ -3,17 +3,18 @@
 # =============================================================================
 import espaloma
 import abc
+import dgl
 
 # =============================================================================
 # MODULE CLASSES
 # =============================================================================
-class Graph(abc.ABC):
-    """ Base class of various graph objects that we host data in.
-    
-    """
+class HomogeneousGraph(espaloma.graph, dgl.Graph):
+    """ Homogeneous graph that contains no more than connectivity and
+    atom attributes.
 
-    @property
-    @abstractmethod
-    def _stage(self):
-        pass
+
+    """
+    def __init__(self):
+        super(HomogeneousGraph, self).__init__()
+
 
