9778015970a058cb83614e6ce7d94397d8abd116
yuanqing-wang <wangyq@wangyq.net>
2020-05-25 14:30:51 -0400

    rewrite __init__

--- espaloma/nn/__init__.py
+++ espaloma/nn/__init__.py
@@ -1,4 +1,5 @@
 import espaloma.nn
-import espaloma.nn.dgl_legacy
+import espaloma.nn.layers
+from espaloma.nn.layers import dgl_legacy
 import espaloma.nn.sequential
 from espaloma.nn.sequential import Sequential
