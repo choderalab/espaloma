cd9e49fa310c5a8e5183e39fd69c45c3cda9d256
yuanqing-wang <wangyq@wangyq.net>
2020-05-26 01:53:26 -0400

    dgl heterograph as attr

--- espaloma/graphs/heterogeneous_graph.py
+++ espaloma/graphs/heterogeneous_graph.py
@@ -8,22 +8,21 @@ import dgl
 # =============================================================================
 # MODULE CLASSES
 # =============================================================================
-class HeterogeneousGraph(esp.Graph, dgl.DGLHeteroGraph):
+class HeterogeneousGraph(esp.Graph):
     r""" Homogeneous graph that contains no more than connectivity and
     atom attributes.
 
     Parameters
     ----------
-    mol : a `rdkit.Chem.Molecule` or `openeye.GraphMol` object
+    homogeneous_graph: `espaloma.HomogeneousGraph` object
+
 
     """
-    def __init__(self, homogeneous_graph=None):
-        # super(HeterogeneousGraph, self).__init__()
-        
-        if homogeneous_graph is not None:
-            self = esp.graphs.utils.read_heterogeneous_graph\
-                    .heterogeneous_graph_from_homogeneous(
-                            homogeneous_graph)
+    def __init__(self, homogeneous_graph):
+    
+        self._graph = esp.graphs.utils.read_heterogeneous_graph\
+                .heterogeneous_graph_from_homogeneous(
+                        homogeneous_graph)
 
 
         
