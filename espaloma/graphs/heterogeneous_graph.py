ab3c283cc1335fda25b44019f0cfb26929c24783
yuanqing-wang <wangyq@wangyq.net>
2020-05-27 01:39:03 -0400

    batch and unbatch heterogeneous graph

--- espaloma/graphs/heterogeneous_graph.py
+++ espaloma/graphs/heterogeneous_graph.py
@@ -6,6 +6,44 @@ import abc
 import dgl
 
 # =============================================================================
+# MODULE FUNCTIONS
+# =============================================================================
+def batch(graphs):
+    r""" Batch multiple heterogenous graph.
+
+    Parameters
+    ----------
+    graphs : list of `esp.HeterogeneousGraph`
+
+    """
+
+    # extract dgl graphs 
+    _graphs = [graph._graph for graph in graphs]
+
+    # batch dgl graph
+    _graphs = dgl.batched_heterograph(_graphs)
+
+    # put batched graph back into HeterogeneousGraph object
+    
+    # TODO: allow more stuff to be done here
+    return HeterogeneousGraph(heterogeneous_graph = _graphs)
+
+
+def unbatch(graph):
+    r""" Unbatch multiple heterogeneous graph.
+
+    """
+
+    # extract dgl graph
+    _graphs = graph._graph
+
+    # unbatch dgl graph
+    _graphs = dgl.unbatch_hetero(_graphs)
+
+    return [HeterogeneousGraph(heterogeneous_graph=g) for g in _graphs]
+
+
+# =============================================================================
 # MODULE CLASSES
 # =============================================================================
 class HeterogeneousGraph(esp.Graph):
@@ -24,7 +62,9 @@ class HeterogeneousGraph(esp.Graph):
 
     """
 
-    def __init__(self, homogeneous_graph):
+    def __init__(self, homogeneous_graph=None, 
+            heterogeneous_graph=None # allow the option to rebuild
+        ):
 
         self._graph = esp.graphs.utils.read_heterogeneous_graph.heterogeneous_graph_from_homogeneous(
             homogeneous_graph
