12b8212cb70b5804782bf42b34662549c00c242b
yuanqing-wang <wangyq@wangyq.net>
2020-05-28 01:58:37 -0400

    loss as attr

--- espaloma/graphs/homogeneous_graph.py
+++ espaloma/graphs/homogeneous_graph.py
@@ -25,13 +25,13 @@ class HomogeneousGraph(esp.Graph, dgl.DGLGraph):
 
         if mol is not None:
             if isinstance(mol, rdkit.Chem.rdchem.Mol):
-                self.from_rdkit(mol)
+                self._from_rdkit(mol)
 
             elif isinstance(mol, openforcefield.topology.molecule.Molecule):
-                self.from_rdkit(mol.to_rdkit())
+                self._from_rdkit(mol.to_rdkit())
 
             elif "oe" in str(type(mol)):  # we don't want to depend on OE
-                self.from_openeye(mol)
+                self._from_openeye(mol)
 
             else:
                 raise RuntimeError(
@@ -40,14 +40,50 @@ class HomogeneousGraph(esp.Graph, dgl.DGLGraph):
                 )
 
 
-    @property
-    def stage(self):
-        return {'type': 'homogeneous'}
+    def _from_rdkit(self, mol):
+        """ API to read RDKit mol.
 
-    def from_rdkit(self, mol):
+        Parameters
+        ----------
+        mol : `rdkit.Chem.rdchem.Mol` object
+
+        """
         # TODO: raise error if this is called after a class has been
         # initialized
         esp.graphs.utils.read_homogeneous_graph.from_rdkit_mol(self, mol)
 
-    def from_openeye(self, mol):
+    def _from_openeye(self, mol):
+        """ API to read OpenEye mol.
+
+        Parameters
+        ----------
+        mol : `openeye.oechem.GraphMol` object
+        """
         esp.graphs.utils.read_homogeneous_graph.from_openeye_mol(self, mol)
+
+    def loss(self, level, *args, **kwargs):
+        """ Loss function between attributes in the graph.
+
+        """
+
+        if level == 'node_classification':
+
+            return self._loss_node_classification(
+                    *args, **kwargs)
+
+        else:
+            raise NotImplementedError
+
+    def legacy_typing(self):
+        assert self.stage['legacy_typed'] == True
+        return self.ndata['legacy_type'] 
+   
+    def gn_typing(self):
+        pass
+
+    def _loss_node_classification(
+            self, 
+            loss_fn=torch.nn.functional.cross_entropy):
+
+        raise NotImplementedError
+                
