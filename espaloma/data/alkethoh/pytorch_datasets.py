6619b2bd8e119f935c4990772ec62589efe11866
Josh Fass <josh.fass@choderalab.org>
2020-06-03 22:03:59 -0400

    add cross-entropy loss as instance method of classification dataset
    per discussion with JDC and YW

--- espaloma/data/alkethoh/pytorch_datasets.py
+++ espaloma/data/alkethoh/pytorch_datasets.py
@@ -4,9 +4,13 @@ from pickle import load
 from typing import Tuple
 
 import numpy as np
+import torch
 from openforcefield.topology import Molecule
 from torch import tensor, Tensor
+from torch.nn import CrossEntropyLoss
 from torch.utils.data.dataset import Dataset
+
+categorical_loss = CrossEntropyLoss()
 
 
 class AlkEthOHDataset(Dataset):
@@ -23,6 +27,13 @@ class AlkEthOHDataset(Dataset):
     def _get_labels(self, mol_name: str, type_name: str) -> Tensor:
         return tensor(self._label_dict[f'{mol_name}_{type_name}_labels'])
 
+    def _get_all_unique_labels(self, type_name: str):
+        all_labels = set()
+        for mol_name in self._mol_names:
+            new_labels = set(self._label_dict[f'{mol_name}_{type_name}_labels'])
+            all_labels.update(new_labels)
+        return sorted(list(all_labels))
+
     def _get_mol_inds_labels(self, index: int, type_name: str) -> Tuple[Molecule, Tensor, Tensor]:
         mol_name = self._mol_names[index]
         mol = self._mols[mol_name]
@@ -30,11 +41,30 @@ class AlkEthOHDataset(Dataset):
         labels = self._get_labels(mol_name, type_name)
         return mol, inds, labels
 
+    def loss(self, index: int, predictions: Tensor) -> Tensor:
+        raise (NotImplementedError)
+
 
 class AlkEthOHAtomTypesDataset(AlkEthOHDataset):
+    def __init__(self):
+        super(AlkEthOHAtomTypesDataset, self).__init__()
+        self.type_name = 'atom'
+        all_labels = self._get_all_unique_labels(self.type_name)
+
+        self._label_mapping = dict(zip(all_labels, range(len(all_labels))))
+        self.n_classes = len(self._label_mapping)
 
     def __getitem__(self, index) -> Tuple[Molecule, Tensor, Tensor]:
-        return self._get_mol_inds_labels(index, 'atom')
+        mol, inds, _labels = self._get_mol_inds_labels(index, self.type_name)
+        labels = tensor([self._label_mapping[int(i)] for i in _labels])
+        return mol, inds, labels
+
+    def loss(self, index: int, predictions: Tensor) -> Tensor:
+        """cross entropy loss"""
+        _, _, target = self[index]
+        assert (predictions.shape == (len(target), self.n_classes))
+
+        return categorical_loss(predictions, target)
 
 
 class AlkEthOHBondTypesDataset(AlkEthOHDataset):
@@ -59,7 +89,16 @@ if __name__ == '__main__':
     # TODO: move this from __main__ into doctests
 
     # atoms
-    print(AlkEthOHAtomTypesDataset()[0])
+    atom_type_dataset = AlkEthOHAtomTypesDataset()
+    n_classes = atom_type_dataset.n_classes
+
+    index = 0
+    n_atoms = atom_type_dataset[index][0].n_atoms
+    predictions = torch.randn(n_atoms, n_classes, requires_grad=True)
+    loss = atom_type_dataset.loss(index, predictions)
+    loss.backward()
+    print(predictions.grad)
+
     # bonds
     print(AlkEthOHBondTypesDataset()[0])
     # angles
