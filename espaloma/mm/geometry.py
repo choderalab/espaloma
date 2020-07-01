16eb6086a10c1273084704946a8c3d3342f036b7
Yuanqing Wang <wangyq@wangyq.net>
2020-07-01 17:02:50 -0400

    geometry function

--- espaloma/mm/geometry.py
+++ espaloma/mm/geometry.py
@@ -2,9 +2,19 @@
 # IMPORTS
 # =============================================================================
 import torch
+import dgl
 
 # =============================================================================
-# MODULE FUNCTIONS
+# UTILITY FUNCTIONS
+# =============================================================================
+def reduce_stack(msg, out):
+    """ Copy massage and stack. """
+    def _reduce_stack(nodes, msg=msg, out=out):
+        return {out: nodes.mailbox[msg]}
+    return _reduce_stack
+
+# =============================================================================
+# SINGLE GEOMETRY ENTITY
 # =============================================================================
 def distance(x0, x1):
     """ Distance. """
@@ -37,3 +47,79 @@ def dihedral(x0, x1, x2, x3):
     left = torch.cross(x1 - x0, x1 - x2)
     right = torch.cross(x2 - x1, x2 - x3)
     return _dihedral(left, right)
+
+# =============================================================================
+# GEOMETRY IN HYPERNODES
+# =============================================================================
+def apply_bond(nodes):
+    """ Bond length in nodes. """
+    return {
+        'x': distance(
+            x0=nodes.data['xyz0'],
+            x1=nodes.data['xyz1']
+        )
+    }
+
+def apply_angle(nodes):
+    """ Angle values in nodes. """
+    return {
+        'x': angle(
+            x0=nodes.data['xyz0'],
+            x1=nodes.data['xyz1'],
+            x2=nodes.data['xyz2']
+        )
+    }
+
+def apply_torsion(nodes):
+    """ Torsion dihedrals in nodes. """
+    return {
+        'x': dihedral(
+            x0=nodes.data['xyz0'],
+            x1=nodes.data['xyz1'],
+            x2=nodes.data['xyz2'],
+            x3=nodes.data['xyz3']
+        )
+    }
+
+
+# =============================================================================
+# GEOMETRY IN GRAPH
+# =============================================================================
+# NOTE:
+# The following functions modify graphs in-place.
+
+def geometry_in_graph(g):
+    """ Assign values to geometric entities in graphs.
+
+    Parameters
+    ----------
+    g : `dgl.DGLHeteroGraph`
+        Input graph.
+
+    Returns
+    -------
+    g : `dgl.DGLHeteroGraph`
+        Output graph.
+
+    Notes
+    -----
+    This function modifies graphs in-place.
+
+    """
+
+    # Copy coordinates to higher-order nodes.
+    g.multi_update_all(
+        {
+            'n1_as_%s_in_n%s' % (pos_idx, big_idx): (
+                dgl.function.copy_src(src='xyz', out='m_xyz%s' % pos_idx),
+                reduce_stack(msg='m_xyz%s' % pos_idx, out='xyz' % pos_idx)
+            ) for big_idx in range(2, 5) for pos_idx in range(big_idx)
+        }
+    )
+
+    # apply geometry functions
+    g.apply_nodes(apply_bond, ntype='n2')
+    g.apply_nodes(apply_angle, ntype='n3')
+    g.apply_nodes(apply_torsion, ntype='n4')
+
+    return g
