9bd87a65eff6e9110a8d33e27099a6d450df1eb2
Josh Fass <josh.fass@choderalab.org>
2020-07-08 14:11:02 -0400

    add alternate parameterization for harmonic potentials

--- espaloma/data/alkethoh/independent_baseline.py
+++ espaloma/data/alkethoh/independent_baseline.py
@@ -1,10 +1,11 @@
 # separate parameters for every atom, bond, angle, torsion, up to symmetry
 
 import numpy as np
-from espaloma.data.alkethoh.data import offmols
 from openeye import oechem
 from openforcefield.topology import Molecule
 from tqdm import tqdm
+
+from espaloma.data.alkethoh.data import offmols
 
 
 def atom_symmetry_classes(offmol: Molecule) -> np.ndarray:
@@ -57,6 +58,8 @@ def get_unique_angles(offmol):
         array of shape (n_angles, 3)
     angle_inds:
         array of shape (n_angles,)
+
+    TODO: refactor to avoid code-duplication between bond, angle, tuple
     """
 
     sym = atom_symmetry_classes(offmol)
@@ -86,6 +89,8 @@ def get_unique_torsions(offmol):
         array of shape (n_angles, 4)
     torsion_inds:
         array of shape (n_angles,)
+
+    TODO: refactor to avoid code-duplication between bond, angle, tuple
     """
 
     sym = atom_symmetry_classes(offmol)
@@ -112,7 +117,29 @@ from espaloma.data.alkethoh.mm_utils import harmonic_bond_potential, harmonic_an
     periodic_torsion_potential
 from espaloma.data.alkethoh.neural_baseline import compute_distances, compute_angles, compute_torsions
 from espaloma.data.alkethoh.neural_baseline import get_snapshots_and_energies
-from espaloma.data.alkethoh.mm_utils import get_bond_energy, get_angle_energy, get_torsion_energy, get_nb_energy
+from espaloma.data.alkethoh.mm_utils import get_bond_energy, get_angle_energy, get_torsion_energy
+
+# Can represent springs a couple different ways
+# 1. In terms of spring constant and equilibrium length
+#   u(r; k, r0) = k/2 (r - r0)^2
+# 2. In terms of a fixed interval of allowable equilibrium lengths [r1, r2]
+#   u(r; k1, k2) = k1 (r - r1)^2 + k2 (r - r2)^2
+#   This has the benefit of allowing us to express variable equilibrium length in terms of
+#   fixed design matrix [(r - r1)^2, (r - r2)^2] and variable coefficients [k1, k2].
+from jax import jit
+
+
+@jit
+def harmonic_bond_potential_alt_param(r, k1, k2, r1, r2):
+    return k1 * (r - r1) ** 2 + k2 * (r - r2) ** 2
+
+
+r1, r2 = 0.0, 0.3
+
+
+@jit
+def harmonic_angle_potential_alt_param(theta, k1, k2, theta1, theta2):
+    return k1 * (theta - theta1) ** 2 + k2 * (theta - theta2) ** 2
 
 
 def compute_harmonic_bond_potential(xyz, params, pair_inds, bond_inds):
@@ -136,6 +163,27 @@ def compute_harmonic_bond_potential(xyz, params, pair_inds, bond_inds):
     return np.sum(harmonic_bond_potential(r, k, r0), axis=1)
 
 
+def compute_harmonic_bond_potential_alt_param(xyz, params, pair_inds, bond_inds):
+    """
+    :param xyz:
+    :param params:
+        array of length 2 * n_unique
+    :param pair_inds:
+        array of shape (len(offmol.bonds), 2)
+    :param bond_inds:
+        numpy array of length len(offmol.bonds),
+        taking integer values in range 0 through n_unique
+    :return:
+    """
+
+    n_unique = int(len(params) / 2)
+    k1s, k2s = params[:n_unique], params[n_unique:]
+    k1, k2 = k1s[bond_inds], k2s[bond_inds]
+
+    r = compute_distances(xyz, pair_inds)
+    return np.sum(harmonic_bond_potential_alt_param(r, k1, k2, r1, r2), axis=1)
+
+
 def compute_harmonic_angle_potential(xyz, params, triple_inds, angle_inds):
     """
 
@@ -156,6 +204,30 @@ def compute_harmonic_angle_potential(xyz, params, triple_inds, angle_inds):
 
     theta = compute_angles(xyz, triple_inds)
     return np.sum(harmonic_angle_potential(theta, k, theta0), axis=1)
+
+
+theta1, theta2 = -np.pi, np.pi
+
+
+def compute_harmonic_angle_potential_alt_param(xyz, params, triple_inds, angle_inds):
+    """
+    :param xyz:
+    :param params:
+        array of length 2 * n_unique
+    :param triple_inds:
+        array of shape (len(offmol.bonds), 2)
+    :param angle_inds:
+        numpy array of length len(offmol.bonds),
+        taking integer values in range 0 through n_unique
+    :return:
+    """
+
+    n_unique = int(len(params) / 2)
+    k1s, k2s = params[:n_unique], params[n_unique:]
+    k1, k2 = k1s[angle_inds], k2s[angle_inds]
+
+    theta = compute_angles(xyz, triple_inds)
+    return np.sum(harmonic_angle_potential_alt_param(theta, k1, k2, theta1, theta2), axis=1)
 
 
 n_periodicities = 6
@@ -201,10 +273,11 @@ if __name__ == '__main__':
     n_unique_bonds = len(set(bond_inds))
     n_bond_params = 2 * n_unique_bonds
     bond_params = np.random.randn(n_bond_params) * 0.01
-    bond_params[:n_unique_bonds] += 10000.0
-    bond_params[n_unique_bonds:] += np.median(distances)
+    bond_params += 5000
+    # bond_params[:n_unique_bonds] += 10000.0
+    # bond_params[n_unique_bonds:] += np.median(distances)
 
-    bond_energies = compute_harmonic_bond_potential(xyz, bond_params, pair_inds, bond_inds)
+    bond_energies = compute_harmonic_bond_potential_alt_param(xyz, bond_params, pair_inds, bond_inds)
     print('bond energies mean', bond_energies.mean())
 
     # angles
@@ -227,6 +300,7 @@ if __name__ == '__main__':
 
     from simtk import unit
     from espaloma.data.alkethoh.mm_utils import get_sim, set_positions, get_energy, get_nb_energy
+
     sim = get_sim(name)
 
     # compare pair inds from harmonic_bond_force and autodiff'd one
@@ -236,7 +310,7 @@ if __name__ == '__main__':
 
     for i in range(harmonic_bond_force.getNumBonds()):
         a, b, length, k = harmonic_bond_force.getBondParameters(i)
-        tup = canonicalize_order((a,b))
+        tup = canonicalize_order((a, b))
         omm_bond_params[tup] = (length, k)
         omm_pair_inds.append(tup)
 
@@ -244,15 +318,13 @@ if __name__ == '__main__':
     print(set(omm_pair_inds))
     print(set(omm_pair_inds).symmetric_difference(set([tuple(p) for p in pair_inds])))
 
-
     # What if I initialize with MM parameters
     for i in range(len(pair_inds)):
         length, k = omm_bond_params[tuple(pair_inds[i])]
-        length_, k_ = length / unit.nanometer, k / (unit.kilojoule_per_mole / (unit.nanometer**2))
+        length_, k_ = length / unit.nanometer, k / (unit.kilojoule_per_mole / (unit.nanometer ** 2))
         print(i, length, k)
-        params[bond_inds[i]] = k_
-        params[bond_inds[i] + n_unique_bonds] = length_
-
+        # params[bond_inds[i]] = k_
+        # params[bond_inds[i] + n_unique_bonds] = length_
 
     # Also initialize angles
     harmonic_angle_force = [f for f in sim.system.getForces() if ("HarmonicAngle" in f.__class__.__name__)][0]
@@ -260,17 +332,16 @@ if __name__ == '__main__':
 
     for i in range(harmonic_angle_force.getNumAngles()):
         a, b, c, theta, k = harmonic_angle_force.getAngleParameters(i)
-        tup = canonicalize_order((a,b,c))
+        tup = canonicalize_order((a, b, c))
         omm_angle_params[tup] = (theta, k)
 
     for i in range(len(triple_inds)):
         theta, k = omm_angle_params[tuple(triple_inds[i])]
-        theta_, k_ = theta / unit.radian, k / (unit.kilojoule_per_mole / (unit.radian**2))
+        theta_, k_ = theta / unit.radian, k / (unit.kilojoule_per_mole / (unit.radian ** 2))
         print(i, theta, k)
         offset = n_unique_bonds * 2
-        params[offset + angle_inds[i]] = k_
-        params[offset + angle_inds[i] + n_unique_angles] = theta_
-
+        # params[offset + angle_inds[i]] = k_
+        # params[offset + angle_inds[i] + n_unique_angles] = theta_
 
     # TODO: train on forces...
 
@@ -288,7 +359,7 @@ if __name__ == '__main__':
         U_torsion = get_torsion_energy(sim)
         U_nb = get_nb_energy(sim)
 
-        assert(np.allclose(U_tot, U_bond + U_angle + U_torsion + U_nb))
+        assert (np.allclose(U_tot, U_bond + U_angle + U_torsion + U_nb))
 
         bond_energies.append(U_bond)
         angle_energies.append(U_angle)
@@ -300,7 +371,8 @@ if __name__ == '__main__':
     angle_target = np.array(angle_energies)
     torsion_target = np.array(torsion_energies)
     nb_target = np.array(nb_energies)
-    valence_target = ani1ccx_energies - nb_target # np.array(valence_energies)
+    valence_target = ani1ccx_energies - nb_target
+    # valence_target = np.array(valence_energies)
     print('bonds', bond_target.mean(), bond_target.std())
     print('angles', angle_target.mean(), angle_target.std())
     print('torsions', torsion_target.mean(), torsion_target.std())
@@ -309,15 +381,20 @@ if __name__ == '__main__':
     # Should check that I can minimize each of these terms independently
 
     from jax import jit
+
+
     @jit
     def loss(all_params):
+
+        # TODO: rescale
 
         # TODO: also include some regularization
         bond_params = all_params[:n_bond_params]
         angle_params = all_params[n_bond_params:(n_bond_params + n_angle_params)]
         torsion_params = all_params[-n_torsion_params:]
 
-        U_bond = compute_harmonic_bond_potential(xyz, bond_params, pair_inds, bond_inds)
+        # U_bond = compute_harmonic_bond_potential(xyz, bond_params, pair_inds, bond_inds)
+        U_bond = compute_harmonic_bond_potential_alt_param(xyz, bond_params, pair_inds, bond_inds)
         U_angle = compute_harmonic_angle_potential(xyz, angle_params, triple_inds, angle_inds)
         U_torsion = compute_periodic_torsion_potential(xyz, torsion_params, quad_inds, torsion_inds)
         U_valence = U_bond + U_angle + U_torsion
@@ -326,7 +403,6 @@ if __name__ == '__main__':
         # return np.std(angle_target - U_angle)
         # return np.std(torsion_target - U_torsion)
         return np.std(valence_target - U_valence)
-
 
 
     from jax import grad
@@ -353,20 +429,23 @@ if __name__ == '__main__':
 
 
     import numpy as onp
+
+
     def jac(params):
         return onp.array(g(params), dtype=onp.float64)
 
 
     from scipy.optimize import minimize, basinhopping
 
-    opt_result = minimize(fun, x0=params, jac=jac, #method='L-BFGS-B',
+    opt_result = minimize(fun, x0=params, jac=jac, method='L-BFGS-B',
                           options=dict(disp=True))
 
     print(opt_result.x[:n_unique_bonds])
-    print(opt_result.x[n_unique_bonds:2*n_unique_bonds])
+    print(opt_result.x[n_unique_bonds:2 * n_unique_bonds])
 
     # try again, but with basinhopping
-    opt_result = basinhopping(fun, x0=opt_result.x, minimizer_kwargs=dict(jac=jac), disp=True)
+    opt_result = basinhopping(fun, x0=opt_result.x, minimizer_kwargs=dict(method='L-BFGS-B', jac=jac), disp=True,
+                              stepsize=100.0)
     print(opt_result.x[:n_unique_bonds])
     print(opt_result.x[n_unique_bonds:2 * n_unique_bonds])
 
