e5bd8f02771ca27dcd4581a90352352b0480f1fc
Yuanqing Wang <wangyq@wangyq.net>
2020-07-08 13:50:54 -0400

    test molecule

--- espaloma/graphs/tests/test_smirnoff.py
+++ espaloma/graphs/tests/test_smirnoff.py
@@ -2,7 +2,13 @@ import pytest
 import espaloma as esp
 
 
-def test_smirnoff():
+def test_smirnoff_esol_first():
     ff = esp.graphs.legacy_force_field.LegacyForceField("smirnoff99Frosst")
     mol = esp.data.esol(first=1)[0].mol
+    mol = ff.parametrize(mol)
+
+
+def test_smirnoff_strange_mol():
+    ff = esp.graphs.legacy_force_field.LegacyForceField("smirnoff99Frosst")
+    mol = esp.Graph('[H]c1c(nc(n(=O)c1N([H])[H])N([H])[H])N2C(C(C(C(C2([H])[H])([H])[H])([H])[H])([H])[H])([H])[H]').mol
     mol = ff.parametrize(mol)
