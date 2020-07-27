dd594b425ed037efed304064dab6e2a4d1948ced
yuanqing-wang <wangyq@wangyq.net>
2020-07-27 14:30:13 -0400

    baseline

--- espaloma/nn/__init__.py
+++ espaloma/nn/__init__.py
@@ -1,6 +1,7 @@
 import espaloma.nn
 import espaloma.nn.layers
 import espaloma.nn.readout
+import espaloma.nn.baselines
 from espaloma.nn.layers import dgl_legacy
 import espaloma.nn.sequential
 from espaloma.nn.sequential import Sequential
