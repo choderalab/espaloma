556615881779062f46a863325648c1f8863bf4c7
yuanqing-wang <wangyq@wangyq.net>
2020-07-27 14:33:45 -0400

    baseline tests

---
+++ espaloma/nn/baselines.py
@@ -0,0 +1,49 @@
+# =============================================================================
+# IMPORTS
+# =============================================================================
+import torch
+import dgl
+
+# =============================================================================
+# MODULE CLASSES
+# =============================================================================
+class FreeParameterBaseline(torch.nn.Module):
+    """ Parametrize a graph by populating the parameters with free
+    `torch.nn.Parameter`.
+
+
+    """
+    def __init__(self, g_ref):
+        super(FreeParameterBaseline, self).__init__()
+        self.g_ref = g_ref
+
+        # whenever there is a reference parameter,
+        # assign a `torch.nn.Parameter`
+        for term in self.g_ref.ntypes:
+            for param, param_value in self.g_ref.nodes[term].data.items():
+                if param.endswith("_ref"):
+                    setattr(
+                        self,
+                        "%s_%s" % (term, param),
+                        torch.nn.Parameter(
+                            torch.zeros_like(
+                                param_value
+                            )
+                        )
+                    )
+
+    def forward(self, g):
+        for term in self.g_ref.ntypes:
+            for param, param_value in self.g_ref.nodes[term].data.items():
+                if param.endswith("_ref"):
+                    g.nodes[term].data[param] = getattr(
+                        self,
+                        "%s_%s",
+                    )
+
+        
+        return g
+
+
+
+
