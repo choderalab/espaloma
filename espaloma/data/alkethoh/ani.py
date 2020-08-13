d97e9e90f33230e8bd587f32ca40d080ede5e73c
Josh Fass <josh.fass@choderalab.org>
2020-08-12 17:16:49 -0700

    save ANI-1ccx forces for all snapshots, replace hartree-to-kcal/mol conversion with call to torchani.units

--- espaloma/data/alkethoh/ani.py
+++ espaloma/data/alkethoh/ani.py
@@ -7,6 +7,10 @@ import numpy as np
 import torch
 import torchani
 from simtk import unit
+from torchani.units import hartree2kcalmol
+
+energy_unit = unit.kilojoule_per_mole
+force_unit = unit.kilojoule_per_mole / unit.nanometer
 
 
 def get_snapshots_and_energies(name='AlkEthOH_r1155'):
@@ -26,8 +30,42 @@ def compute_ani_energies(snapshots: md.Trajectory):
     species = model.species_to_tensor(species_string).unsqueeze(0)
 
     coordinates = torch.tensor([sample for sample in xyz_in_angstroms], dtype=torch.float32)
-    energy = model((torch.stack([species[0]] * len(xyz_in_angstroms)), coordinates))
-    return energy.energies.detach().numpy() * 627.5 * unit.kilocalorie_per_mole  # convert from hartree to kcal/mol
+    _, energy = model((torch.stack([species[0]] * len(xyz_in_angstroms)), coordinates))  # hartree
+    return hartree2kcalmol(energy.detach().numpy()) * unit.kilocalorie_per_mole
+
+
+def compute_ani_forces(snapshots: md.Trajectory):
+    xyz_in_angstroms = (snapshots.xyz * unit.nanometer).value_in_unit(unit.angstrom)
+    species_string = ''.join([a.element.symbol for a in snapshots.topology.atoms])
+
+    species = model.species_to_tensor(species_string).unsqueeze(0)
+
+    coordinates = torch.tensor([sample for sample in xyz_in_angstroms], dtype=torch.float32, requires_grad=True)
+
+    _, energy = model((torch.stack([species[0]] * len(xyz_in_angstroms)), coordinates))  # hartree
+
+    forces = -torch.autograd.grad(energy.sum(), coordinates, create_graph=True, retain_graph=True)[
+        0]  # hartree per angstrom
+
+    return hartree2kcalmol(forces.detach().numpy()) * unit.kilocalorie_per_mole / unit.angstrom
+
+
+def compute_ani_energies_and_forces(snapshots: md.Trajectory):
+    xyz_in_angstroms = (snapshots.xyz * unit.nanometer).value_in_unit(unit.angstrom)
+    species_string = ''.join([a.element.symbol for a in snapshots.topology.atoms])
+
+    species = model.species_to_tensor(species_string).unsqueeze(0)
+
+    coordinates = torch.tensor([sample for sample in xyz_in_angstroms], dtype=torch.float32, requires_grad=True)
+
+    _, energy = model((torch.stack([species[0]] * len(xyz_in_angstroms)), coordinates))  # hartree
+
+    forces = -torch.autograd.grad(energy.sum(), coordinates, create_graph=True, retain_graph=True)[
+        0]  # hartree per angstrom
+
+    energies = hartree2kcalmol(energy.detach().numpy()) * unit.kilocalorie_per_mole
+    forces = hartree2kcalmol(forces.detach().numpy()) * unit.kilocalorie_per_mole / unit.angstrom
+    return energies, forces
 
 
 model = torchani.models.ANI1ccx()
@@ -43,11 +81,17 @@ if __name__ == '__main__':
 
     for name in tqdm(names):
         snapshots, energies = get_snapshots_and_energies(name)
-        ani_energies = compute_ani_energies(snapshots)
-        ani_energies_kjmol = ani_energies.value_in_unit(unit.kilojoule_per_mole)
-        np.save('snapshots_and_energies/{}_ani1ccx_energies'.format(name), ani_energies_kjmol)
+        ani_energies, ani_forces = compute_ani_energies_and_forces(snapshots)
 
-        # note: these will include a large additive offset: interested in stddev of these residuals
+        # save energies
+        ani_energies_in_omm_units = ani_energies.value_in_unit(energy_unit)
+        np.save('snapshots_and_energies/{}_ani1ccx_energies'.format(name), ani_energies_in_omm_units)
+
+        # save forces
+        ani_forces_in_omm_units = ani_forces / force_unit
+        np.save('snapshots_and_energies/{}_ani1ccx_forces'.format(name), ani_forces_in_omm_units)
+
+        # print openff vs. ani residual stddev
         mm_energies_kjmol = (energies * unit.kilojoule_per_mole).value_in_unit(unit.kilojoule_per_mole)
-        residuals = ani_energies_kjmol - mm_energies_kjmol
+        residuals = ani_energies_in_omm_units - mm_energies_kjmol
         print('stddev(residuals): {:.4f} kJ/mol'.format(np.std(residuals)))
