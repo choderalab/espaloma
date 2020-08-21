75129e88483f4f51e1d1d5d5ec98642d944dfed3
Yuanqing Wang <wangyq@wangyq.net>
2020-08-20 22:00:59 -0400

    qcarchive data

--- espaloma/data/__init__.py
+++ espaloma/data/__init__.py
@@ -6,6 +6,7 @@ import espaloma.data.dataset
 import espaloma.data.md
 import espaloma.data.normalize
 import espaloma.data.utils
+import espaloma.data.qcarchive_utils
 from espaloma.data.collection import *
 
 # esol = utils.from_csv(os.path.dirname(utils.__file__) + "/esol.csv")
