1911511053a4488388c4a4811cca2892f70dc62c
Josh Fass <josh.fass@choderalab.org>
2020-08-21 13:49:56 -0700

    apply to torsion perception task

--- espaloma/nn/factornet.py
+++ espaloma/nn/factornet.py
@@ -133,7 +133,10 @@ class MLP(nn.Module):
 
 
 class AtomToFactor(nn.Module):
-    def __init__(self, msg_src_name, msg_dest_name, current_repr_name, updated_repr_name, atom_dim, initial_factor_dims=dict(bond=1, angle=1, torsion=1), updated_factor_dims=dict(bond=10, angle=10, torsion=10)):
+    def __init__(self, msg_src_name, msg_dest_name, current_repr_name, updated_repr_name, atom_dim,
+                 initial_factor_dims=dict(bond=1, angle=1, torsion=1), updated_factor_dims=dict(bond=10, angle=10, torsion=10),
+                 symmetrize_torsion=False
+                 ):
         """
         Parameters
         ----------
@@ -151,6 +154,9 @@ class AtomToFactor(nn.Module):
             dimensions of the current factor representations
         updated_factor_dims : dict(string -> int)
             dimensions of the updated factor representations
+        symmetrize_torsion : bool
+            whether to compute torsion representation in way that enforces
+            f(a,b,c,d) = f(d,c,b,a)
         """
         super(AtomToFactor, self).__init__()
         self.msg_src_name = msg_src_name
@@ -161,6 +167,7 @@ class AtomToFactor(nn.Module):
         self.atom_dim = atom_dim
         self.initial_factor_dims = initial_factor_dims
         self.updated_factor_dims = updated_factor_dims
+        self.symmetrize_torsion = symmetrize_torsion
 
 
         # bonds
@@ -248,20 +255,24 @@ class AtomToFactor(nn.Module):
         
         g.nodes['angle'].data[self.updated_repr_name] = self.angle_f(X_f) + self.angle_f(X_r)
         
-    def _compute_updated_torsion_representation(self, g):
+    def _compute_updated_torsion_representation(self, g, symmetrize=False):
         """if a torsion with current representation r is connected to atoms (a,b,c,d),
         
-        update representation to f(a, b, c, d; r) + f(d, c, b, a; r)
+        symmetrize -> update representation to f(a, b, c, d; r) + f(d, c, b, a; r)
+        not symmetrize -> update representation to f(a, b, c, d; r)
+
         """
         
         current_repr = g.nodes['torsion'].data[self.current_repr_name]
         
         incoming_messages = [g.nodes['torsion'].data[f'{self.msg_dest_name}[{i}]'] for i in range(4)]
-        
         X_f = torch.cat(incoming_messages + [current_repr], dim=1)
-        X_r = torch.cat(incoming_messages[::-1] + [current_repr], dim=1)
-        
-        g.nodes['torsion'].data[self.updated_repr_name] = self.torsion_f(X_f) + self.torsion_f(X_r)
+
+        if symmetrize:
+            X_r = torch.cat(incoming_messages[::-1] + [current_repr], dim=1)
+            g.nodes['torsion'].data[self.updated_repr_name] = self.torsion_f(X_f) + self.torsion_f(X_r)
+        else:
+            g.nodes['torsion'].data[self.updated_repr_name] = self.torsion_f(X_f)
 
     def forward(self, g):
         self._pass_labeled_messages_from_atom_to_bond(g)
@@ -271,13 +282,10 @@ class AtomToFactor(nn.Module):
         self._compute_updated_angle_representation(g)
 
         self._pass_labeled_messages_from_atom_to_torsion(g)
-        self._compute_updated_torsion_representation(g)
+        self._compute_updated_torsion_representation(g, symmetrize=self.symmetrize_torsion)
 
 
 # TODO: Also add rings...
-
-# TODO: FactorToAtom class
-#   an atom can talk to an unpredictable number of factors, so aggregation will have to be simpler
 
 """
 something like, updated_atom_repr = g(\sum_{neighboring_bond_factors} f(factor_repr, atom_repr);  \sum_{neighboring_angle_factors} f(factor_repr, atom_repr); \sum_{neighboring_torsion_factors} f(factor_repr, atom_repr) )
@@ -399,4 +407,74 @@ if __name__ == '__main__':
     offmol = Molecule.from_smiles('C1CCCCC1')
     factor_graph = offmol_to_heterograph(offmol)
 
-    factor_net = FactorNet(ATOM_DIM)
+    factor_net = FactorNet(ATOM_DIM, n_rounds=1)
+    factor_net.forward(factor_graph)
+
+    from espaloma.data.alkethoh.pytorch_datasets import AlkEthOHTorsionTypesDataset
+
+    torsions_dataset = AlkEthOHTorsionTypesDataset()
+
+
+    # one-hot-ify
+    all_torsion_labels = set()
+    for (_, _, labels) in torsions_dataset:
+        all_torsion_labels.update(set(labels.detach().numpy()))
+    all_torsion_labels = sorted(list(all_torsion_labels))
+    n_types = len(all_torsion_labels)
+    print(all_torsion_labels)
+    print(n_types)
+    to_index = dict(zip(all_torsion_labels, range(n_types)))
+
+    def labels_to_tensor(labels):
+        labels = labels.detach().numpy()
+        return torch.tensor([to_index[l] for l in labels], dtype=torch.int64)
+
+    # convert to heterographs
+    print('convert to heterographs')
+    from tqdm import tqdm
+
+    graphs = []
+    for (mol, inds, labels) in torsions_dataset:
+        g = offmol_to_heterograph(mol)
+        n_torsions = g.number_of_nodes('torsion')
+        #g.nodes['torsion'].data['label'] = torch.randint(0, n_types, (n_torsions,))
+        g.nodes['torsion'].data['label'] = labels_to_tensor(labels)
+        graphs.append(g)
+
+    print('batching')
+    g = dgl.batch_hetero(graphs[:10])
+    readout = MLP(factor_net.factor_dims['torsion'], n_types)
+
+    from torch.nn import CrossEntropyLoss
+
+    def loss(g):
+        factor_net.forward(g)
+        # TODO: could measure cross entropy loss from multiple rounds
+        preds = readout(g.nodes['torsion'].data['round1_repr'])
+        target = g.nodes['torsion'].data['label']
+        return CrossEntropyLoss()(preds, target)
+
+    print('computing loss once')
+    L = loss(g)
+    print(L)
+
+    print('training with Adam')
+    from torch.optim.adam import Adam
+    optimizer = Adam(list(readout.parameters()) + list(factor_net.parameters()), lr=1e-3)
+    loss_traj = []
+    for t in range(1000):
+        print('iteration: ', t)
+        optimizer.zero_grad()
+        batch = dgl.batch_hetero([graphs[i] for i in np.random.randint(0, len(graphs), 100)])
+        L = loss(batch)
+        L.backward()
+        optimizer.step()
+
+        loss_traj.append(L)
+
+        preds = readout(batch.nodes['torsion'].data['round1_repr']).argmax(1)
+        targets = batch.nodes['torsion'].data['label']
+        accuracy = np.mean((preds == targets).detach().numpy())
+        print('\tloss = {:.3f}'.format(L.detach().numpy()))
+        print('\taccuracy = {:.3f}'.format(accuracy))
+
