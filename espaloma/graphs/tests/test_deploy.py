75e8919be84520f2fccbcb83d421ed1ebaff8757
Josh Fass <josh.fass@choderalab.org>
2020-10-08 10:56:08 -0400

    test naming

--- espaloma/graphs/tests/test_deploy.py
+++ espaloma/graphs/tests/test_deploy.py
@@ -1,9 +1,12 @@
 import pytest
 import espaloma as esp
 
-def test_methane():
+def test_butane():
+    """check that esp.graphs.deploy.openmm_system_from_graph runs without error on butane"""
     ff = esp.graphs.legacy_force_field.LegacyForceField("smirnoff99Frosst")
     g = esp.Graph('CCCC')
     g = ff.parametrize(g)
     print(g.mol)
     esp.graphs.deploy.openmm_system_from_graph(g, suffix="_ref")
+
+# TODO: test that desired parameters are assigned
