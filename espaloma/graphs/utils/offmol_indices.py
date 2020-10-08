cdee07ecd66324acf76fe9376f7208a858dbc308
Josh Fass <josh.fass@choderalab.org>
2020-10-08 14:04:32 -0400

    add improper torsion indices

--- espaloma/graphs/utils/offmol_indices.py
+++ espaloma/graphs/utils/offmol_indices.py
@@ -5,10 +5,14 @@ from openforcefield.topology import Molecule
 def bond_indices(offmol: Molecule) -> np.ndarray:
     return np.array([(b.atom1_index, b.atom2_index) for b in offmol.bonds])
 
+
 def angle_indices(offmol: Molecule) -> np.ndarray:
     return np.array(sorted([tuple([atom.molecule_atom_index for atom in angle]) for angle in offmol.angles]))
+
 
 def proper_torsion_indices(offmol: Molecule) -> np.ndarray:
     return np.array(sorted([tuple([atom.molecule_atom_index for atom in proper]) for proper in offmol.propers]))
 
-# TODO: improper torsion indices
+
+def improper_torsion_indices(offmol: Molecule) -> np.ndarray:
+    return np.array(sorted([tuple([atom.molecule_atom_index for atom in improper]) for improper in offmol.impropers]))
