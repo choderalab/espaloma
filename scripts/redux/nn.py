1818352108a29eab526981d507de2c5c8963ba2c
Josh Fass <josh.fass@choderalab.org>
2020-10-12 17:18:37 -0400

    ({in_features|in_feats}, h_feats, {out_features|num_classes}) --> (in_dim, hidden_dim, out_dim)

--- scripts/redux/nn.py
+++ scripts/redux/nn.py
@@ -5,11 +5,11 @@ from torch import nn
 
 
 class MLP(nn.Module):
-    def __init__(self, in_features, out_features, activation=F.relu):
+    def __init__(self, in_dim, out_dim, activation=F.relu):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(in_features, 64)
+        self.fc1 = nn.Linear(in_dim, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, out_features)
+        self.fc3 = nn.Linear(64, out_dim)
         self.activation = activation
 
     def forward(self, x):
@@ -19,11 +19,11 @@ class MLP(nn.Module):
 
 
 class TAG(nn.Module):
-    def __init__(self, in_feats, h_feats, num_classes, k=2, activation=F.relu):
+    def __init__(self, in_dim, hidden_dim, out_dim, k=2, activation=F.relu):
         super(TAG, self).__init__()
-        self.layer1 = TAGConv(in_feats, h_feats, k, activation=activation)
-        self.layer2 = TAGConv(h_feats, h_feats, k, activation=activation)
-        self.layer3 = TAGConv(h_feats, num_classes, k, activation=activation)
+        self.layer1 = TAGConv(in_dim, hidden_dim, k, activation=activation)
+        self.layer2 = TAGConv(hidden_dim, hidden_dim, k, activation=activation)
+        self.layer3 = TAGConv(hidden_dim, out_dim, k, activation=activation)
         self.activation = activation
 
     def forward(self, graph, inputs):
