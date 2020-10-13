15e7845eb1258cb7728794da84e7fb4983644029
Josh Fass <josh.fass@choderalab.org>
2020-10-13 17:52:16 -0400

    use periodic_fixed_phases in compute_propers and compute_impropers

--- espaloma/redux/energy.py
+++ espaloma/redux/energy.py
@@ -20,26 +20,25 @@ def compute_angles(xyz: torch.Tensor, params: ParameterizedSystem, indices: Indi
 
 
 def compute_propers(xyz: torch.Tensor, params: ParameterizedSystem, indices: Indices) -> torch.Tensor:
-    # TODO: reduce code duplication
+    # TODO: reduce code duplication with compute_impropers
     a, b = xyz[:, indices.propers[:, 0]], xyz[:, indices.propers[:, 1]]
     c, d = xyz[:, indices.propers[:, 2]], xyz[:, indices.propers[:, 3]]
     dihedrals = esp.mm.geometry.dihedral(a, b, c, d)
     ks = params.propers
-    return esp.mm.torsion.periodic_torsion(dihedrals, ks)
+    return esp.mm.functional.periodic_fixed_phases(dihedrals, ks)
 
 
 def compute_impropers(xyz: torch.Tensor, params: ParameterizedSystem, indices: Indices) -> torch.Tensor:
-    # TODO: reduce code duplication
+    # TODO: reduce code duplication with compute_propers
     a, b = xyz[:, indices.impropers[:, 0]], xyz[:, indices.impropers[:, 1]]
     c, d = xyz[:, indices.impropers[:, 2]], xyz[:, indices.impropers[:, 3]]
     dihedrals = esp.mm.geometry.dihedral(a, b, c, d)
     ks = params.impropers
-    return esp.mm.torsion.periodic_torsion(dihedrals, ks)
+    return esp.mm.functional.periodic_fixed_phases(dihedrals, ks)
 
 
 def compute_valence_energy(offmol: Molecule, xyz: torch.Tensor, params: ParameterizedSystem) -> torch.Tensor:
     indices = Indices(offmol)
     harmonic_terms = compute_bonds(xyz, params, indices).sum(1) + compute_angles(xyz, params, indices).sum(1)
     torsion_terms = compute_propers(xyz, params, indices).sum(1) + compute_impropers(xyz, params, indices).sum(1)
-    # TODO: currently doesn't run because esp.mm.torsion.periodic_torsion is encountering a runtime dimension error
     return harmonic_terms + torsion_terms
