1dfa364ac2957c0bdd25b855b34b6d5d1daea276
Josh Fass <josh.fass@choderalab.org>
2020-10-14 12:04:14 -0400

    generate 4-particle configurations with dihedral angles uniformly distributed in [0, 2pi]

--- espaloma/mm/tests/test_dihedral.py
+++ espaloma/mm/tests/test_dihedral.py
@@ -1,7 +1,55 @@
 import numpy as np
 import numpy.testing as npt
-import pytest
 import torch
+
+
+def _sample_unit_circle(n_samples: int = 1) -> np.ndarray:
+    """
+    >>> np.isclose(np.linalg.norm(_sample_unit_circle(1)), 1)
+    True
+
+    """
+    theta = np.random.rand(n_samples) * 2 * np.pi
+    x = np.cos(theta)
+    y = np.sin(theta)
+    xy = np.array([x, y]).T
+    assert (xy.shape == (n_samples, 2))
+    return xy
+
+
+def _sample_four_particle_torsion_scan(n_samples: int = 1) -> np.ndarray:
+    """Generate n_samples random configurations of a 4-particle system abcd where
+    * distances ab, bc, cd are constant,
+    * angles abc, bcd are constant
+    * dihedral angle abcd is uniformly distributed in [0, 2pi]
+
+    Returns
+    -------
+    xyz : np.ndarray, shape = (n_samples, 4, 3)
+
+    Notes
+    -----
+    * Positions of a,b,c are constant, and x-coordinate of d is constant.
+        To be more exacting, could add random displacements and rotations.
+    """
+    a = (-3, -1, 0)
+    b = (-2, 0, 0)
+    c = (-1, 0, 0)
+    d = (0, 1, 0)
+
+    # form one 3D configuration
+    conf = np.array([a, b, c, d])
+    assert (conf.shape == (4, 3))
+
+    # make n_samples copies
+    xyz = np.array([conf] * n_samples, dtype=float)
+    assert (xyz.shape == (n_samples, 4, 3))
+
+    # assign y and z coordinates of particle d to unit-circle samples
+    xyz[:, 3, 1:] = _sample_unit_circle(n_samples)
+
+    return xyz
+
 
 
 def test_dihedral_vectors():
