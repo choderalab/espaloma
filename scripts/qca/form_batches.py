edf70911dd9140e4363cbfe9187a791c40a999c2
Josh Fass <josh.fass@choderalab.org>
2020-10-20 16:58:35 -0400

    move dataset selection out of form_batches.py, for subsequent reuse

--- scripts/qca/form_batches.py
+++ scripts/qca/form_batches.py
@@ -1,14 +1,5 @@
 import qcportal
-
-dataset_type = 'OptimizationDataset'
-
-optimization_datasets = {
-    "Roche": "OpenFF Gen 2 Opt Set 1 Roche",
-    "Coverage": "OpenFF Gen 2 Opt Set 2 Coverage",
-    "Pfizer": "OpenFF Gen 2 Opt Set 3 Pfizer Discrepancy",
-    "eMolecules": "OpenFF Gen 2 Opt Set 4 eMolecules Discrepancy",
-    "Bayer": "OpenFF Gen 2 Opt Set 5 Bayer",
-}
+from dataset_selection import optimization_datasets, dataset_type
 
 batch_size = 100
 
