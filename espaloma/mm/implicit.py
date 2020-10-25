dfce74c4e0c7ebbdd48c90766951101858342333
Josh Fass <josh.fass@choderalab.org>
2020-10-24 23:25:55 -0400

    port @proteneer's GBSA implementation instead

--- espaloma/mm/implicit.py
+++ espaloma/mm/implicit.py
@@ -16,81 +16,80 @@ def step(x):
 
 
 def gbsa_obc2_energy(
-        distance_matrix: torch.tensor,
-        radii: torch.tensor,
-        scales: torch.tensor,
-        charges: torch.tensor,
-        offset=0.009, screening=138.935484,
+        distance_matrix_in_bohr,
+        radii_in_bohr, scales, charges,
+        alpha=0.8, beta=0.0, gamma=2.909125,
+        dielectric_offset=0.009,
         surface_tension=28.3919551,
-        solvent_dielectric=78.5, solute_dielectric=1.0
+        solute_dielectric=1.0,
+        solvent_dielectric=78.5,
+        probe_radius=0.14
 ):
     """
     ported from jax/numpy implementation here:
     https://github.com/openforcefield/bayes-implicit-solvent/blob/067239fcbb8af28eb6310d702804887662692ec2/bayes_implicit_solvent/gb_models/jax_gb_models.py#L13-L60
+
+    with corrections and refinements by Yutong Zhao here
+    https://github.com/proteneer/timemachine/blob/417f4b0b1181b638935518532c78c380b03d7d19/timemachine/potentials/gbsa.py#L1-L111
     """
     # convert distances and radii into units of nanometers before proceeding
-    distance_matrix_in_nm = distance_matrix * distance_to_nm
-    radii_in_nm = radii * distance_to_nm
+    distance_matrix = distance_matrix_in_bohr * distance_to_nm
+    radii = radii_in_bohr * distance_to_nm
 
-    # scales are unitless
+    N = len(charges)
+    eye = torch.eye(N, dtype=distance_matrix.dtype)
 
-    N = len(radii_in_nm)
-    eye = torch.eye(N, dtype=distance_matrix_in_nm.dtype)
-    r = distance_matrix_in_nm + eye
-    or1 = radii_in_nm.reshape((N, 1)) - offset
-    or2 = radii_in_nm.reshape((1, N)) - offset
+    r = distance_matrix + eye
+    or1 = radii.reshape((N, 1)) - dielectric_offset
+    or2 = radii.reshape((1, N)) - dielectric_offset
     sr2 = scales.reshape((1, N)) * or2
 
-    L = torch.max(or1, torch.abs(r - sr2))
+    L = torch.max(or1, abs(r - sr2))
     U = r + sr2
 
-    # https://github.com/openforcefield/bayes-implicit-solvent/blob/067239fcbb8af28eb6310d702804887662692ec2/bayes_implicit_solvent/gb_models/jax_gb_models.py#L29-L31
-    #  but incorporating bugfix Yutong pointed out
-    I = 1 / L \
-        - 1 / U \
-        + 0.25 * (r - sr2 ** 2 / r) * (1 / (U ** 2) - 1 / (L ** 2)) \
-        + 0.5 * torch.log(L / U) / r
+    I = 1 / L - 1 / U + 0.25 * (r - sr2 ** 2 / r) * (
+            1 / (U ** 2) - 1 / (L ** 2)) + 0.5 * torch.log(
+        L / U) / r
+    # handle the interior case
     I = torch.where(or1 < (sr2 - r), I + 2 * (1 / or1 - 1 / L), I)
-    I = step(r + sr2 - or1) * 0.5 * I
-
+    I = step(r + sr2 - or1) * 0.5 * I  # note the extra 0.5 here
     I -= torch.diag(torch.diag(I))
-    I = torch.sum(I, dim=1)
+    I = torch.sum(I, axis=1)
 
     # okay, next compute born radii
-    offset_radius = radii_in_nm - offset
-    psi = I * offset_radius
-    psi_coefficient = 0.8
-    psi2_coefficient = 0
-    psi3_coefficient = 2.909125
+    offset_radius = radii - dielectric_offset
 
-    psi_term = (psi_coefficient * psi) + (psi2_coefficient * psi ** 2) + (
+    psi = I * offset_radius
+
+    psi_coefficient = alpha
+    psi2_coefficient = beta
+    psi3_coefficient = gamma
+
+    psi_term = (psi_coefficient * psi) - (psi2_coefficient * psi ** 2) + (
             psi3_coefficient * psi ** 3)
 
-    B = 1 / (1 / offset_radius - torch.tanh(psi_term) / radii_in_nm)
+    B = 1 / (1 / offset_radius - torch.tanh(psi_term) / radii)
 
-    # finally, compute the three energy terms
     E = 0.0
-
     # single particle
-    E += torch.sum(surface_tension * (radii_in_nm + 0.14) ** 2 * (radii_in_nm / B) ** 6)
-    E += torch.sum(-0.5 * screening * (
+    # ACE
+    E += torch.sum(
+        surface_tension * (radii + probe_radius) ** 2 * (radii / B) ** 6)
+
+    # on-diagonal
+    E += torch.sum(-0.5 * (
             1 / solute_dielectric - 1 / solvent_dielectric) * charges ** 2 / B)
 
     # particle pair
     # note: np.outer --> torch.ger
-    f = torch.sqrt(
-        r ** 2 + torch.ger(B, B) * torch.exp(-r ** 2 / (4 * torch.ger(B, B))))
+    f = torch.sqrt(r ** 2 + torch.ger(B, B) * torch.exp(
+        -r ** 2 / (4 * torch.ger(B, B))))
     charge_products = torch.ger(charges, charges)
 
-    E += torch.sum(
-        torch.triu(
-            -screening * (
-                    + 1 / solute_dielectric
-                    - 1 / solvent_dielectric
-            ) * charge_products / f,
-            diagonal=1
-        )
-    )
+    ixns = - (
+            1 / solute_dielectric - 1 / solvent_dielectric) * charge_products / f
+
+    E += torch.sum(torch.triu(ixns, diagonal=1))
 
     # E is in kJ/mol at this point
     # return E in espaloma energy unit
