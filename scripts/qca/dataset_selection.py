7bde2e167f14e9b60b488914eba94f464a9321e6
Josh Fass <josh.fass@choderalab.org>
2020-10-26 10:09:37 -0400

    add OpenFF Benchmark Set to dataset_selection

--- scripts/qca/dataset_selection.py
+++ scripts/qca/dataset_selection.py
@@ -6,4 +6,5 @@ optimization_datasets = {
     "Pfizer": "OpenFF Gen 2 Opt Set 3 Pfizer Discrepancy",
     "eMolecules": "OpenFF Gen 2 Opt Set 4 eMolecules Discrepancy",
     "Bayer": "OpenFF Gen 2 Opt Set 5 Bayer",
+    "Benchmark": "OpenFF Benchmark Set",
 }
