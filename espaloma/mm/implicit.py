eb73857498c0bb101513f2f13c9d86471d71605a
Josh Fass <josh.fass@choderalab.org>
2020-10-29 10:45:33 -0400

    increase descriptiveness in gbsa implementation, add thorough shape assertions
    Thanks to @yuanqing-wang for carefully stepping through this with me
    
    Co-Authored-By: Yuanqing Wang <26657667+yuanqing-wang@users.noreply.github.com>

--- espaloma/mm/implicit.py
+++ espaloma/mm/implicit.py
@@ -36,23 +36,51 @@ def _gbsa_obc2_energy_omm(
 
     N = len(charges)
     eye = torch.eye(N, dtype=distance_matrix.dtype)
+    assert (eye.shape == (N, N))
 
     r = distance_matrix + eye
+    assert (r.shape == (N, N))
     or1 = radii.reshape((N, 1)) - dielectric_offset
     or2 = radii.reshape((1, N)) - dielectric_offset
-    sr2 = scales.reshape((1, N)) * or2
 
-    L = torch.max(or1, abs(r - sr2))
+    assert (or1.shape == (N, 1))
+    assert (or2.shape == (1, N))
+
+    sr2 = scales.reshape((1, N)) * or2
+    assert (sr2.shape == (1, N))
+
+    r_sr2 = abs(r - sr2)
+    assert (r_sr2.shape == (N, N))
+
+    L = torch.max(or1, r_sr2)
+    # TODO: check if elementwise
+    print('L.shape', L.shape)
+    print('or1.shape', or1.shape)
+    print('r_sr2.shape', r_sr2.shape)
+    # TODO: this is fishy
+    # assert(L.shape == or1.shape)
+
     U = r + sr2
+    assert (U.shape == (N, N))
 
     I = 1 / L - 1 / U + 0.25 * (r - sr2 ** 2 / r) * (
             1 / (U ** 2) - 1 / (L ** 2)) + 0.5 * torch.log(
         L / U) / r
+    assert (I.shape == (N, N))
+
     # handle the interior case
-    I = torch.where(or1 < (sr2 - r), I + 2 * (1 / or1 - 1 / L), I)
+    condition = or1 < (sr2 - r)
+    if_true = I + 2 * (1 / or1 - 1 / L)
+    if_false = I
+    I = torch.where(condition, if_true, if_false)
+
     I = step(r + sr2 - or1) * 0.5 * I  # note the extra 0.5 here
+
+    # intention: zero out values on the diagonal
+    # TODO: possibly replace this diag(diag) with scatter update
     I -= torch.diag(torch.diag(I))
-    I = torch.sum(I, axis=1)
+
+    I = torch.sum(I, dim=1) 
 
     # okay, next compute born radii
     offset_radius = radii - dielectric_offset
@@ -67,28 +95,39 @@ def _gbsa_obc2_energy_omm(
             psi3_coefficient * psi ** 3)
 
     B = 1 / (1 / offset_radius - torch.tanh(psi_term) / radii)
+    assert (B.shape == (N,))
 
     E = 0.0
     # single particle
     # ACE
-    E += torch.sum(
-        surface_tension * (radii + probe_radius) ** 2 * (radii / B) ** 6)
+    ACE_individual_terms = surface_tension * (radii + probe_radius) ** 2 * (
+                radii / B) ** 6
+    ACE = torch.sum(ACE_individual_terms)
+    assert (ACE_individual_terms.shape == (N,))
+    E += ACE
 
     # on-diagonal
-    E += torch.sum(-0.5 * (
-            1 / solute_dielectric - 1 / solvent_dielectric) * charges ** 2 / B)
+    on_diagonal = -0.5 * (
+            1 / solute_dielectric - 1 / solvent_dielectric) * charges ** 2 / B
+    assert (on_diagonal.shape == (N,))
+    E += torch.sum(on_diagonal)
 
     # particle pair
     # note: np.outer --> torch.ger
+    assert (B.ndim == 1)
+
     f = torch.sqrt(r ** 2 + torch.ger(B, B) * torch.exp(
         -r ** 2 / (4 * torch.ger(B, B))))
     charge_products = torch.ger(charges, charges)
+    assert (f.shape == (N, N))
+    assert (charge_products.shape == (N, N))
 
     ixns = - (
             1 / solute_dielectric - 1 / solvent_dielectric) * charge_products / f
 
     E += torch.sum(torch.triu(ixns, diagonal=1))
     return E  # E is in kJ/mol at this point
+
 
 def gbsa_obc2_energy(
         distance_matrix_in_bohr,
@@ -100,6 +139,7 @@ def gbsa_obc2_energy(
     distance_matrix = distance_matrix_in_bohr * distance_to_nm
     radii = radii_in_bohr * distance_to_nm
 
-    E = _gbsa_obc2_energy_omm(distance_matrix, radii, scales, charges, alpha, beta, gamma, **kwargs)
+    E = _gbsa_obc2_energy_omm(distance_matrix, radii, scales, charges, alpha,
+                              beta, gamma, **kwargs)
 
-    return E * energy_from_kjmol # return E in espaloma energy unit
+    return E * energy_from_kjmol  # return E in espaloma energy unit
