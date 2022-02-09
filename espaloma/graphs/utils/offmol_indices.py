c3da06ebaa3276863cc10440fa47ef787e98db29
kaminow <benjamin.kaminow@choderalab.org>
2022-02-09 11:48:51 -0500

    Minor refactoring.

--- espaloma/graphs/utils/offmol_indices.py
+++ espaloma/graphs/utils/offmol_indices.py
@@ -61,37 +61,31 @@ def improper_torsion_indices(offmol: Molecule, improper_def='espaloma') -> np.nd
     Motivation: offmol.impropers returns a large number of impropers, and we may wish to restrict this number.
     May update this filter definition based on discussion in https://github.com/openff.toolkit/openff.toolkit/issues/746
     """
-    ## Check options
-    if (improper_def != 'espaloma') and (improper_def != 'smirnoff'):
-        raise ValueError(f'Unknown value for improper_def: {improper_def}')
 
     ## Find all atoms bound to exactly 3 other atoms
-    ## This finds all orderings, which is what we want for the espaloma case
-    ##  but not for smirnoff
-    improper_smarts = "[*:1]~[X3:2](~[*:3])~[*:4]"
-    mol_idxs = offmol.chemical_environment_matches(improper_smarts)
-
     if improper_def == 'espaloma':
+        ## This finds all orderings, which is what we want for the espaloma case
+        ##  but not for smirnoff
+        improper_smarts = '[*:1]~[X3:2](~[*:3])~[*:4]'
+        mol_idxs = offmol.chemical_environment_matches(improper_smarts)
         return np.array(mol_idxs)
+    elif improper_def == 'smirnoff':
+        improper_smarts = '[*:2]~[X3:1](~[*:3])~[*:4]'
+        ## For smirnoff ordering, we only want to find the unique combinations
+        ##  of atoms forming impropers so we can permute them the way we want
+        mol_idxs = offmol.chemical_environment_matches(improper_smarts,
+            unique=True)
 
-    ## Get all unique improper centers, and the list of atoms that they
-    ##  bind to
-    imp_centers = {}
-    for idxs in mol_idxs:
-        center_atom = idxs[1]
-        if center_atom in imp_centers:
-            continue
-        other_atoms = tuple(sorted([idxs[0]]+list(idxs[2:])))
-        imp_centers[center_atom] = other_atoms
+        ## Get all ccw orderings
+        # feels like there should be some good way to do this with itertools...
+        idx_permuts = []
+        for c, *other_atoms in mol_idxs:
+            for i in range(3):
+                idx = [c]
+                for j in range(3):
+                    idx.append(other_atoms[(i+j)%3])
+                idx_permuts.append(tuple(idx))
 
-    ## Get all ccw orderings
-    # feels like there should be some good way to do this with itertools...
-    mol_idxs = []
-    for c, other_atoms in imp_centers.items():
-        for i in range(3):
-            idx = [c]
-            for j in range(3):
-                idx.append(other_atoms[(i+j)%3])
-            mol_idxs.append(tuple(idx))
-
-    return np.array(mol_idxs)
+        return np.array(idx_permuts)
+    else:
+        raise ValueError(f'Unknown value for improper_def: {improper_def}')
