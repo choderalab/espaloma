4a8d9706be6dc8bc1bbb43775554087b8eee434b
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-07-14 06:38:44 -0700

    test for model fetch

---
+++ espaloma/utils/tests/test_model_fetch.py
@@ -0,0 +1,15 @@
+import espaloma as esp
+import torch
+from espaloma.utils.model_fetch import get_model
+from openff.toolkit.topology import Molecule
+
+def test_get_model(tmp_path):
+    model_dir = tmp_path / "latest"
+    model_path = get_model(model_dir=model_dir, disable_progress_bar=True)
+
+    molecule = Molecule.from_smiles("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
+    molecule_graph = esp.Graph(molecule)
+
+    espaloma_model = torch.load(model_path)
+    espaloma_model.eval()
+    espaloma_model(molecule_graph.heterograph)
